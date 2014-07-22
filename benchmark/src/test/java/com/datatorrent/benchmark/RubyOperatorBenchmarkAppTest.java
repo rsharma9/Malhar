b49cfc15e5b120e3c89e3f2a06660d3e7bf07952
Rohit Sharma <rohitsharma91@gmail.com>
2014-07-22 12:36:30 -0700

    ruby operator and benchmark application

---
+++ benchmark/src/test/java/com/datatorrent/benchmark/RubyOperatorBenchmarkAppTest.java
@@ -0,0 +1,18 @@
+package com.datatorrent.benchmark;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Test;
+
+import com.datatorrent.api.LocalMode;
+
+public class RubyOperatorBenchmarkAppTest {
+
+	@Test
+	public void testApplication() throws Exception
+	{
+		LocalMode lma = LocalMode.newInstance();
+		new RubyOperatorBenchmarkApplication().populateDAG(lma.getDAG(), new Configuration(false));
+		LocalMode.Controller lc = lma.getController();
+		lc.run(10000);
+	}
+}
